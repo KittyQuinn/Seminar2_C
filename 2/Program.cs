e8a8b6a7bb3a7eab0b97bcb25c5024d47d5ab896
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-07 21:28:09 +0200

    Added 2nd task

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿// из трехзначного числа удаляет вторую цифру 456 -> 46
+
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+
+int a1 = num / 100;
+int a2 = num % 10;
+
+Console.WriteLine(num);
+
+int result = a1 * 10 + a2;
+
+
+Console.WriteLine(result);
