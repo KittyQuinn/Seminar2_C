bb47219e4e5b016853746ead1d6dac11b8f0ea8a
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-07 20:59:36 +0200

    Added 1st task

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// случайное число от 10 до 99 и показывает наибольшее число 
+
+Console.Clear();
+
+int num = new Random().Next(10,100);
+int a1 = num/10;
+int a2 = num%10;
+
+int max = a1;
+if (a2>max) max = a2;
+
+
+Console.WriteLine(num);
+
+Console.WriteLine(max);
