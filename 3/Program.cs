2a46cba5bb93a7a991269b9d661f87fb775e4970
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-07 22:32:23 +0200

    Added 3

---
+++ 3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Write number1 ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Write number2 ");
+int number2 = int.Parse(Console.ReadLine());
+
+int d = number1%number2;
+
+   if (d == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+
+    else 
+    {
+        Console.WriteLine($"Не кратно, остаток {d},");
+    }
