8a8a28f8649ef07ccf961ca70640135f6b451b27
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-07 22:58:51 +0200

    Added 4

---
+++ 4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Write the number ");
+int num = int.Parse(Console.ReadLine());
+
+if ((num%7==0) & (num%23==0))
+{
+    Console.WriteLine("Yes");
+}
+
+else 
+{
+    Console.WriteLine("No");
+}
