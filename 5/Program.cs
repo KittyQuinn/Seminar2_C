51f9449021ccdc1428f952011aad5f36069eee10
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-07 23:25:38 +0200

    Added 5

---
+++ 5/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Write number1 ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Write number2 ");
+int b = int.Parse(Console.ReadLine());
+
+if ((a == b*b) || (b == a*a))
+{
+    Console.WriteLine("Yes");
+}
+
+else
+{
+    Console.WriteLine("No");
+}
